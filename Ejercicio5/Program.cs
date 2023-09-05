4b70ff010857b3287ed456cd450144d26da58a45
JulianSuarez5 <Juacosuarez12@gmail.com>
2023-09-04 22:29:33 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class NumerosDescendentes
+{
+    static void Main()
+    {
+        int i;
+
+        for (i = 10; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadKey();
+    }
+}
+
